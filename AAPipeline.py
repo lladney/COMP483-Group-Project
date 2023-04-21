2a2193a00f4a776c5710c03fcb41e2736ebdaf87
lladney <125703033+lladney@users.noreply.github.com>
2023-04-21 20:26:58 +0000

    Update AAPipeline.py

--- AAPipeline.py
+++ AAPipeline.py
@@ -15,8 +15,8 @@ today = date.today()
 #input_file = "C:/Users/param/Downloads/sars-cov-2-prot1.txt"
 input_file = open('proteinSearch.txt', 'w')  # open and write sequences to proteins text file 
 output_file = "/Users/laraladney/Documents/sars-cov2_amino_acid_frequencies-prot1.csv"
-matrix_output_file = "/Users/laraladney/Downloads/matrix_out.png"
-matrix_values_file = "/Users/laraladney/Downloads/matrix_values.csv"
+#matrix_output_file = "/Users/laraladney/Downloads/matrix_out.png"
+#matrix_values_file = "/Users/laraladney/Downloads/matrix_values.csv"
 #matrix_output_file = "C:/Users/param/Downloads/matrix_out.png"
 #matrix_values_file = "C:/Users/param/Downloads/matrix_values.csv"
 
@@ -24,7 +24,7 @@ matrix_values_file = "/Users/laraladney/Downloads/matrix_values.csv"
 Entrez.email = input("Enter email: ")   # user prompted to enter email (tell NCBI who you are to access sequences)
 
 protTerm = input("Enter NCBI search term: ")     # user prompted to enter protein sequence ID
-numSeqs = input("How many protein sequences would you like to extract?") # user prompted to enter # seqs
+numSeqs = input("How many protein sequences would you like to extract? ") # user prompted to enter # seqs
 
 print("Default date range for protein sequence extraction is from 01/01/2000 - Current.")
 print("Would you like to extract protein sequences from a specified date range?")
@@ -49,7 +49,16 @@ input_file.write(record.rstrip('\n'))
 input_file.close()
 
 '''
-# CODE TO RETRIEVE SINGLE PROTEIN SEQUENCE FROM NCBI USING PROTEIN ID
+# CODE TO RETREVE PROTEIN SEQUENCES BASED ON URL
+print("Analysis of protein sequences will be performed on search results from uniprot.org.")
+url = input("Please enter URL of search results: ")     # user prompted to enter protein sequence ID
+all_fastas = requests.get(url).text
+fasta_list = re.split(r'\n(?=>)', all_fastas)
+[fasta for fasta in fasta_list if '>' in fasta]
+fastas = '\n'.join(fasta_list)
+input_file.write(fastas)
+
+# CODE TO RETRIEVE SINGLE PROTEIN SEQUENCE FROM NCBI
 Entrez.email = input("Enter email: ")   # user prompted to enter email (tell NCBI who you are to access sequences)
 seqID = input("Enter protein ID: ")     # user prompted to enter protein sequence ID
 handle = Entrez.efetch(db="protein", id=seqID, rettype="gb", retmode="fasta") # handle to fetch protein sequence (user input)
@@ -57,6 +66,8 @@ record = SeqIO.read(handle, "genbank")  # use SeqIO parser to read handle (in ge
 sequence = str(record.seq)              # save sequence from record as a string
 input_file.write(sequence)              # write protein sequence to input file
 '''
+
+
 
 # need to write to outfile:
 # amino acids with the same/most similar frequency across the variants (least likely to mutate)
@@ -72,6 +83,7 @@ def extract_amino_acids(input_file):
         sequence = str(record.seq)
         protein = ProteinAnalysis(sequence)
         aa_percentages = protein.get_amino_acids_percent()
+        amino_acids.append(aa_percentages)
     return amino_acids
 
 # Define function to write amino acid frequencies and min/max percentages to a CSV file
@@ -79,8 +91,10 @@ def write_csv(output_file, data):
     with open(output_file, "w", newline="") as csvfile:
         writer = csv.writer(csvfile)
         writer.writerow(["Rank", "Amino Acid", "Frequency"])
-        rank = 1 
+        rank = 1
         for aa, freq in data.items():
+#            min_freq = min(amino_acids, key=lambda x: x[aa])[aa]
+#            max_freq = max(amino_acids, key=lambda x: x[aa])[aa]
             writer.writerow([rank, aa, freq])
             rank += 1
 
@@ -102,6 +116,7 @@ for k, v in total_aa_freqs.items():
     total_aa_freqs[k] = v/int(numSeqs)
     
 total_aa_freqs_sorted = dict(sorted(total_aa_freqs.items(), key = lambda item: item[1], reverse = True))
+#rankdict = dict(zip(total_aa_freqs_sorted.keys(), rankdata([-i for i in total_aa_freqs_sorted.values()], method='ordinal')))
             
 # Calculate Min/Max %'s of Frequencies
 min_percent = min(total_aa_freqs.values())
@@ -119,7 +134,7 @@ plt.xlabel("Amino Acid")
 plt.ylabel("Frequency")
 plt.text(-0.6, min_percent-0.005, f"Min: {min_percent:.2%}")
 plt.text(19.2, max_percent-0.005, f"Max: {max_percent:.2%}")
-
+'''
 # Create a matrix of amino acid frequencies
 index_table = np.zeros((len(total_aa_freqs), len(amino_acids)))
 aa_indices = {aa: i for i, aa in enumerate(total_aa_freqs)}
@@ -141,5 +156,5 @@ plt.title("Amino Acid Frequency Matrix in SARS-CoV-2 Proteins")
 plt.xlabel("Protein Sequence Position")
 plt.ylabel("Amino Acid")
 plt.savefig(matrix_output_file)
-
+'''
 plt.show()
