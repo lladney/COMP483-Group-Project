f31d049e0883232d6421cae6b79950f01c27aa38
Walter McCain III <48781009+mccainwa@users.noreply.github.com>
2023-04-24 23:26:42 -0500

    Update AAPipe3.py

--- AAPipe3.py
+++ AAPipe3.py
@@ -1,6 +1,5 @@
 import sys
 import csv
-import os
 import numpy as np
 from Bio import SeqIO
 from Bio import Entrez
@@ -11,35 +10,26 @@ import scipy.stats
 import re
 import pandas as pd
 from datetime import date
+
 today = date.today()
 
 # Define input and output files
-#input_file = "C:/Users/param/Downloads/sars-cov-2-prot1.txt"
 input_file = open('proteinSearch.txt', 'w')  # open and write sequences to proteins text file 
 output_file = "/Users/param/Downloads/sars-cov2_amino_acid_frequencies-prot1.csv"
 output_file2 = "/Users/param/Downloads/aa_freqs_per_prot.csv"
 
 # Protein Sequence Retrieval from NCBI based on search term
-Entrez.email = "wmccain@luc.edu"   # user prompted to enter email (tell NCBI who you are to access sequences)
+Entrez.email = "lladney@luc.edu" # user prompted to enter email (tell NCBI who you are to access sequences)
 
-protTerm = "sars cov 2"     # user prompted to enter protein sequence ID
-#numSeqs = 10 # user prompted to enter # seqs
-numSeqs = int(input("Enter number of sequences: "))
-#numSeqs = int(numSeqs.strip())
+protTerm = "sars cov 2" # user prompted to enter protein sequence ID
+numSeqs = 10 # user prompted to enter # seqs
 
-#print("Default date range for protein sequence extraction is from 01/01/2000 - Current.")
-#print("Would you like to extract protein sequences from a specified date range?")
-#dateY_N = input("Enter (Y/N): ")
 dateY_N = "N"
 startDate = ""
 endDate = ""
 if dateY_N == "N" or "n":
     startDate == "2000/01/01" and endDate == today.strftime("%d/%m/%Y")
-'''
-if dateY_N == "Y" or "y":
-    startDate == input("Using format YYYY/MM/DD, enter start date: ")
-    endDate == input("Using format YYYY/MM/DD, enter end date: ")
-'''
+
 searchResultHandle = Entrez.esearch(db = "protein", term = protTerm, retmax = numSeqs, mindate = startDate, maxdate = endDate)
 searchResult = Entrez.read(searchResultHandle)
 ids = searchResult["IdList"]
@@ -51,32 +41,21 @@ input_file = open('proteinSearch.txt', 'w')
 input_file.write(record.rstrip('\n'))
 input_file.close()
 
-'''
-# CODE TO RETRIEVE SINGLE PROTEIN SEQUENCE FROM NCBI
-Entrez.email = input("Enter email: ")   # user prompted to enter email (tell NCBI who you are to access sequences)
-seqID = input("Enter protein ID: ")     # user prompted to enter protein sequence ID
-handle = Entrez.efetch(db="protein", id=seqID, rettype="gb", retmode="fasta") # handle to fetch protein sequence (user input)
-record = SeqIO.read(handle, "genbank")  # use SeqIO parser to read handle (in genbank format)
-sequence = str(record.seq)              # save sequence from record as a string
-input_file.write(sequence)              # write protein sequence to input file
-'''
-
-# need to write to outfile:
-# amino acids with the same/most similar frequency across the variants (least likely to mutate)
-# amino acids with larges ranges of frequencies across the variants (most likely to mutate)
-# table containing shapiro wilk test, skewness, kurtosis
 
 input_file = open('proteinSearch.txt', 'r')
 
-# Define function to extract amino acid sequences from a FASTA file
+# Define function to extract amino acid sequences and protein IDs from a FASTA file
 def extract_amino_acids(input_file):
+    records = SeqIO.parse(input_file, "fasta")
     amino_acids = []
-    for record in SeqIO.parse(input_file, "fasta"):
+    for record in records:
+        protein_id = record.description.split("|")
+        if len(protein_id) > 1:
+            protein_id = protein_id[1]
+        else:
+            protein_id = record.id
         sequence = str(record.seq)
-        protein = ProteinAnalysis(sequence)
-        aa_percentages = protein.get_amino_acids_percent()
-        aa_percentages['id'] = record.id  # add protein ID to the dictionary
-        amino_acids.append(aa_percentages)
+        amino_acids.append((protein_id, sequence))
     return amino_acids
 
 # Define function to write amino acid frequencies and min/max percentages to a CSV file
@@ -85,8 +64,7 @@ def write_csv(output_file, data):
         writer = csv.writer(csvfile)
         writer.writerow(["Rank", "Amino Acid", "Frequency"])
         rank = 1
-        sorted_data = sorted(data.items(), key=lambda x: x[1], reverse=True)
-        for aa, freq in sorted_data:
+        for aa, freq in data.items():
             writer.writerow([rank, aa, freq])
             rank += 1
 
@@ -95,51 +73,55 @@ amino_acids = extract_amino_acids(input_file)
 
 # Close input file
 input_file.close()                     
-
+    
 # Calculate total frequencies of each amino acid
 total_aa_freqs = {}
 for aa in amino_acids:
-    for k, v in aa.items():
-        if k in total_aa_freqs:
-            total_aa_freqs[k] += v
-        else:
-            total_aa_freqs[k] = v
+    for dict_item in aa:
+        if isinstance(dict_item, dict):
+            for k, v in dict_item.items():
+                if k in total_aa_freqs:
+                    total_aa_freqs[k] += v
+                else:
+                    total_aa_freqs[k] = v
 
 for k, v in total_aa_freqs.items():
     total_aa_freqs[k] = v/int(numSeqs)
-    
+
 total_aa_freqs_sorted = dict(sorted(total_aa_freqs.items(), key = lambda item: item[1], reverse = True))
-#rankdict = dict(zip(total_aa_freqs_sorted.keys(), rankdata([-i for i in total_aa_freqs_sorted.values()], method='ordinal')))
-            
+
 # Calculate Min/Max %'s of Frequencies
-min_percent = min(total_aa_freqs.values())
-max_percent = max(total_aa_freqs.values())
+if len(total_aa_freqs) > 0:
+    min_percent = min(total_aa_freqs.values())
+    max_percent = max(total_aa_freqs.values())
+else:
+    min_percent = 0
+    max_percent = 0
 
 # Write amino acid frequencies to output file
 write_csv(output_file, total_aa_freqs_sorted)
 
-
-# Write amino acid frequencies per protein to output file
+# Define function to write amino acid frequencies and protein IDs to a CSV file
 def write_csv2(output_file2, data):
     with open(output_file2, "w", newline="") as csvfile:
         writer = csv.writer(csvfile)
         writer.writerow(["Protein ID", "Amino Acid", "Frequency"])
-        for aa in data:
-            for k, v in aa.items():
-                if k != 'id':
-                    writer.writerow([aa['id'], k, v])
-                    
-# Write amino acid frequencies per protein to output file
-write_csv2(output_file2, amino_acids)
+        for protein_id, aa in data:
+            for aa_name, freq in aa.items():
+                writer.writerow([protein_id, aa, freq])
 
-aa_outfile = open("test.outs.csv", "w", newline="")
+aa_outfile = open("test.out2.csv", "w", newline="")
 writer = csv.writer(aa_outfile, delimiter=",")
 
-header = ["Protein ID"] + list(amino_acids[0].keys())
+header = ["Protein ID"]
+if isinstance(amino_acids[0][1], dict):
+    header += list(amino_acids[0][1].keys())
 writer.writerow(header)
 
 for aa_dict in amino_acids:
-    row = [aa_dict.get("id")] + list(aa_dict.values())
+    row = [aa_dict[0]]
+    if isinstance(aa_dict[1], dict):
+        row += list(aa_dict[1].values())
     writer.writerow(row)
 
 aa_outfile.close()
