1970d81730a2de75a44479d521505dd0843b1b0c
Walter McCain III <48781009+mccainwa@users.noreply.github.com>
2023-04-27 18:59:45 -0500

    Create CorrelationMs.R

---
+++ CorrelationMs.R
@@ -0,0 +1,20 @@
+# Load in and Install Library
+install.packages("corrplot")
+library(corrplot)
+
+# Load the csv file using the read.csv function.
+data <- read.csv("C:/Users/param/OneDrive - Loyola University Chicago/Computational Bio Comp383/complete.test.out.csv")
+
+# Create a correlation matrix using the cor function.
+corr_matrix <- cor(data[,2:21])
+
+# Create the heatmap using the corrplot function.
+corrplot(corr_matrix, type = "upper", method = "color", 
+         tl.col = "black", tl.srt = 45)
+
+# This will create a boxplot for each amino acid, showing the distribution of frequencies for each one. 
+# You can compare the boxplots to see if there are any significant differences in the frequency distributions between the amino acids.
+boxplot(data[,2], data[,3], data[,4], data[,5], data[,6], data[,7], data[,8], data[,9], data[,10], data[,11], 
+        data[,12], data[,13], data[,14], data[,15], data[,16], data[,17], data[,18], data[,19], data[,20], data[,21],
+        col = c("#1f77b4", "#aec7e8", "#ff7f0e", "#ffbb78", "#2ca02c", "#98df8a", "#d62728", "#ff9896", "#9467bd", "#c5b0d5",
+                "#8c564b", "#c49c94", "#e377c2", "#f7b6d2", "#7f7f7f", "#c7c7c7", "#bcbd22", "#dbdb8d", "#17becf", "#9edae5"))
